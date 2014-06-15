166eb73a6f2e6014082a960be64f8bfdf7bf6538
wolfoux <wolfoux@hotmail.com>
2014-06-15 15:54:36 +0200

    Commig aveclesoleil t

--- app/cache/prod/jms_security/soleil-ArticleBundle-Controller-ArticleController.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('C:46:"JMS\\SecurityExtraBundle\\Metadata\\ClassMetadata":114:{a:5:{i:0;s:49:"soleil\\ArticleBundle\\Controller\\ArticleController";i:1;a:0:{}i:2;a:0:{}i:3;a:0:{}i:4;i:1402831636;}}');
